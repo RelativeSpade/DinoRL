fa477f3ce5816738599d898821a7710d09781cfd
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-04 07:02:43 -0400

    random testing model

--- scripts/main.py
+++ scripts/main.py
@@ -16,4 +16,12 @@ from scripts.WebGame import WebGame
 
 env = WebGame()
 
-env.reset()
+for episode in range(10):
+    obs = env.reset()
+    done = False
+    total_reward = 0
+
+    while not done:
+        obs, reward, done, info = env.step(env.action_space.sample())
+        total_reward += reward
+    print("Episode {}: Total Reward: {}".format(episode, total_reward))
