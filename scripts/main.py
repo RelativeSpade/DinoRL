821b7053baffb104f7b1ec32a3a6a978d6c3cfbe
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-04 04:48:58 -0400

    imports

---
+++ scripts/main.py
@@ -0,0 +1,14 @@
+# MSS (Multiple Screen Shots) for Screen Capture
+from mss import mss
+# Sending commands
+import pydirectinput
+# Image Recognition
+import cv2
+# Process game over screen
+import pytesseract
+import numpy as np
+from matplotlib import pyplot as plt
+import time
+from gym import Env
+from gym.spaces import Box, Discrete
+
