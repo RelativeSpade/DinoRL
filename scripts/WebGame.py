eda663c053d0c8e81adbc1fa2e9ee315922661c6
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-04 04:53:20 -0400

    empty web game environment

---
+++ scripts/WebGame.py
@@ -0,0 +1,27 @@
+from gym import Env
+
+
+class WebGame(Env):
+    # Setup the environment
+    def __init__(self):
+        pass
+
+    # Called to do something in the game
+    def step(self, action):
+        pass
+
+    # Restart the game
+    def reset(self):
+        pass
+
+    # Visualize the Game
+    def render(self):
+        pass
+
+    # Get a segment of game to observe
+    def get_observation(self):
+        pass
+
+    # Get the "Game Over" text
+    def get_done(self):
+        pass
