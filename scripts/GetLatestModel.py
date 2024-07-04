ee5bddd3c852a50e954ac1367890430e768784d0
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-04 07:04:32 -0400

    get latest implemented

---
+++ scripts/GetLatestModel.py
@@ -0,0 +1,18 @@
+import os
+import re
+
+
+def get_latest_model(checkpoint_dir):
+    pattern = re.compile(r'Dino(\d+)')
+    checkpoint_files = [
+        (file, int(pattern.search(file).group(1)))
+        for file in os.listdir(checkpoint_dir)
+        if pattern.search(file)
+    ]
+    if not checkpoint_files:
+        print('No checkpoint found')
+        return None, 0
+
+    latest_checkpoint = max(checkpoint_files, key=lambda x: x[1])
+    print('Latest checkpoint:', latest_checkpoint[0])
+    return os.path.join(checkpoint_dir, latest_checkpoint[0]), latest_checkpoint[1]
