c6da8750035026bb09e705d15f0b470f2b01ac0b
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-04 07:14:23 -0400

    Should be done.

--- scripts/main.py
+++ scripts/main.py
@@ -12,16 +12,23 @@ import time
 from gym import Env
 from gym.spaces import Box, Discrete
 
+from scripts import GetLatestModel
+from scripts.TrainAndLoggingCallback import TrainAndLoggingCallback
 from scripts.WebGame import WebGame
+from stable_baselines3 import DQN
 
 env = WebGame()
 
-for episode in range(10):
-    obs = env.reset()
-    done = False
-    total_reward = 0
+CHECKPOINT_DIR = '../trains'
+LOG_DIR = '../logs'
 
-    while not done:
-        obs, reward, done, info = env.step(env.action_space.sample())
-        total_reward += reward
-    print("Episode {}: Total Reward: {}".format(episode, total_reward))
+callback = TrainAndLoggingCallback(check_freq=1000, save_path=CHECKPOINT_DIR)
+
+LATEST_MODEL, NUM = GetLatestModel
+
+if LATEST_MODEL is None:
+    model = DQN('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=1, buffer_size=1200000, learning_starts=1000)
+else:
+    model = DQN.load(LATEST_MODEL, env)
+
+    model.learn(total_timesteps=100000, callback=callback)
