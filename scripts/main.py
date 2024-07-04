8ce00868483d2c13f511c98b584404efd9739b55
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-04 17:50:37 -0400

    Perfect!

--- scripts/main.py
+++ scripts/main.py
@@ -1,34 +1,33 @@
-# MSS (Multiple Screen Shots) for Screen Capture
-from mss import mss
-# Sending commands
-import pydirectinput
-# Image Recognition
-import cv2
-# Process game over screen
-import pytesseract
-import numpy as np
-from matplotlib import pyplot as plt
-import time
-from gym import Env
-from gym.spaces import Box, Discrete
-
-from scripts import GetLatestModel
+import pygetwindow as gw
+from stable_baselines3 import DQN
+from GetLatestModel import get_latest_model
 from scripts.TrainAndLoggingCallback import TrainAndLoggingCallback
 from scripts.WebGame import WebGame
-from stable_baselines3 import DQN
 
-env = WebGame()
 
-CHECKPOINT_DIR = '../trains'
-LOG_DIR = '../logs'
+def is_dino_tab_active():
+    active_window = gw.getActiveWindow()
+    if active_window:
+        return 'chrome://dino/' in active_window.title
+    return False
 
-callback = TrainAndLoggingCallback(check_freq=1000, save_path=CHECKPOINT_DIR)
 
-LATEST_MODEL, NUM = GetLatestModel
+if is_dino_tab_active():
+    print(f'Dino is open')
+    env = WebGame()
 
-if LATEST_MODEL is None:
-    model = DQN('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=1, buffer_size=1200000, learning_starts=1000)
-else:
-    model = DQN.load(LATEST_MODEL, env)
+    CHECKPOINT_DIR = '../trains'
+    LOG_DIR = '../logs'
+
+    callback = TrainAndLoggingCallback(check_freq=1000, save_path=CHECKPOINT_DIR)
+
+    LATEST_MODEL, NUM = get_latest_model(CHECKPOINT_DIR)
+
+    if LATEST_MODEL is None:
+        model = DQN('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=1, buffer_size=1200000, learning_starts=1000)
+    else:
+        model = DQN.load(LATEST_MODEL, env)
 
     model.learn(total_timesteps=100000, callback=callback)
+else:
+    print(f'Dino is not open')
